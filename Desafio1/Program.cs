0dcc5e8723a527ab748283acfe3985674193ea6b
Gilberto Martins <gilbertomartins643@gmail.com>
2022-03-10 14:53:09 -0300

    Desafio Ho Ho Ho

---
+++ Desafio1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Desafio1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Digite um valor: ");
+            int N = int.Parse(Console.ReadLine());
+
+            //Exibir "Ho" do papai noel
+            for (int i = 0; i < N; i++)
+            {
+                if (i != (N - 1))
+                {
+                    Console.Write("Ho ");
+                }
+                else
+                {
+                    Console.WriteLine("Ho!");
+                }
+            }
+        }
+    }
+}
