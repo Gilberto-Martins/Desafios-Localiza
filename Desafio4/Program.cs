9b48c7997602b3a8c43e5aeb553d53998919a209
Gilberto Martins <gilbertomartins643@gmail.com>
2022-03-10 15:16:52 -0300

    Desafio Xenlonguinho

---
+++ Desafio4/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Desafio4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numero = Convert.ToInt32(Console.ReadLine());
+            
+            while(numero-- > 0)
+            {
+              int valor = Convert.ToInt32(Console.ReadLine());
+              int value = (int)Math.Sqrt(valor);
+              int resp = valor - value;
+              
+              Console.WriteLine(resp);
+            }
+        }
+    }
+}
