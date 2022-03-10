d8c935f2b77858682f3d597e582fc5e06934c495
Gilberto Martins <gilbertomartins643@gmail.com>
2022-03-10 15:31:53 -0300

    Desafio Pedro Bento e o Mundo de OZ

---
+++ Desafio5/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace Desafio5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var joias = new List<string>();
+
+            while (true)
+            {
+                string recJoia = Console.ReadLine();
+
+                if(!string.IsNullOrWhiteSpace(recJoia))
+                {
+                    joias.Add(recJoia);
+                }
+                else
+                {
+                    break;
+                }
+            }
+            var totalJoias = joias.Distinct().ToList();
+
+            Console.WriteLine(totalJoias.Count);
+        }
+    }
+}
