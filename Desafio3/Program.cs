60736584aafdfea8de76666858c91126f5b1c976
Gilberto Martins <gilbertomartins643@gmail.com>
2022-03-10 15:13:16 -0300

    Desafio Esfera

---
+++ Desafio3/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Desafio3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double pi, raio, volume;
+            pi = 3.14159;
+
+            Console.Write("Digite o valor do Raio: ");
+            raio = Convert.ToDouble(Console.ReadLine());
+
+            raio = Math.Pow(raio, 3.0);
+
+            volume = (4/3.0) * pi * raio;
+
+            Console.WriteLine($"Volume : {volume.ToString("f3")}");
+        }
+    }
+}
